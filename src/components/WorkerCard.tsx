ceae7379e03c58b6cb176c691131919e628ff901
Adhi602 <adityakonnur6@gmail.com>
2025-11-27 14:03:28 +0530

    Change navigation to worker profile

--- src/components/WorkerCard.tsx
+++ src/components/WorkerCard.tsx
@@ -120,7 +120,7 @@ export const WorkerCard = ({ worker }: WorkerCardProps) => {
       <div className="flex gap-2">
         <Button 
           className="flex-1" 
-          onClick={() => navigate(`/worker/${worker.id}`)}
+          onClick={() => navigate("/worker-profile")}
         >
           View Profile
         </Button>
